60447df919241b7a04b8137b13761e65a2e2cb89
Roy <826343@cognizant.com>
2020-07-27 20:49:03 +0530

    mini application completed

--- query/index.js
+++ query/index.js
@@ -1,37 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
   if ((type === "commentCreated")) {
-    const { id, content, postId ,status} = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
-    post.comments.push({ id, content ,status});
+    post.comments.push({ id, content, status });
   }
   if ((type === "commentUpdated")) {
-    const { id, content, postId ,status} = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
-    const comment = post.comments.find((c)=>c.id === id);
-    comment.status= status;
+    const comment = post.comments.find((c) => c.id === id);
+    comment.status = status;
     comment.content = content;
   }
-  console.log(JSON.stringify(posts,undefined,2))
+}
 
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data)
+  console.log(JSON.stringify(posts, undefined, 2))
   res.send({ status: "success" });
 });
-app.listen(4002, () => {
+app.listen(4002, async () => {
+  const res = await axios.get('http://localhost:4005/events');
+  for (let event of res.data) {
+    console.log('Processing Event');
+    handleEvent(event.type, event.data)
+  }
   console.log("listening on 4002");
 });
